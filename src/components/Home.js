071210e0251777a4de587e6bc5d42990272218e1
Dean Spooner <dean.spooner@me.com>
2021-10-16 23:27:28 +0100

    Placeholder main page content and styling.

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Home.css";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <h1 className="UkanaHeader">Ukana</h1>
+      <h1 className="UkanaHeaderJp">言う仮名</h1>
+      <h3 className="Subheader">Your place for Japanese language-learning</h3>
+    </div>
+  );
+};
+
+export default Home;
