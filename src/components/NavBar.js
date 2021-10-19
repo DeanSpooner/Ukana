8b632d1026a3051ce8abea5afcb0eff64cdc6653
Dean Spooner <dean.spooner@me.com>
2021-10-19 21:44:30 +0100

    Added links to Navbar for graded Kanji 1-6 and 8.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,14 +7,52 @@ const NavBar = () => {
   return (
     <div className="Navbar">
       <ul className="navbar-links">
-        <li className="navbar-links-item">
+        <li>
           <Link className="item" to="/ukana">
             <img src={ukanaLogo} alt="Ukana logo" className="ukanaLogo"></img>
           </Link>
         </li>
         <li>
           <Link className="item non-home" to="/ukana/random">
-            <p className="NavLink">Random Kanji</p>
+            <p className="navLink">Random Kanji</p>
+          </Link>
+        </li>
+        <li className="item non-home">
+          <p className="navLink gradedKanji">Graded Kanji:</p>
+        </li>
+        <li>
+          <Link className="item non-home" to="/ukana/random">
+            <p className="navLink numbered">1</p>
+          </Link>
+        </li>
+        <li>
+          <Link className="item non-home" to="/ukana/random">
+            <p className="navLink numbered">2</p>
+          </Link>
+        </li>
+        <li>
+          <Link className="item non-home" to="/ukana/random">
+            <p className="navLink numbered">3</p>
+          </Link>
+        </li>
+        <li>
+          <Link className="item non-home" to="/ukana/random">
+            <p className="navLink numbered">4</p>
+          </Link>
+        </li>
+        <li>
+          <Link className="item non-home" to="/ukana/random">
+            <p className="navLink numbered">5</p>
+          </Link>
+        </li>
+        <li>
+          <Link className="item non-home" to="/ukana/random">
+            <p className="navLink numbered">6</p>
+          </Link>
+        </li>
+        <li>
+          <Link className="item non-home" to="/ukana/random">
+            <p className="navLink numbered">8</p>
           </Link>
         </li>
       </ul>
