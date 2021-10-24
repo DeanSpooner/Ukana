6d1bad451c9479f42aaa4f72ee3d2b525fb7ac20
Dean Spooner <dean.spooner@me.com>
2021-10-24 12:28:28 +0100

    Added button to refresh random Kanji.

--- src/components/RandomKanji.js
+++ src/components/RandomKanji.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
-import "../styles/Home.css";
+import "../styles/KanjiCard.css";
 import KanjiCard from "./KanjiCard";
 import GetRandomKanji from "../requests/GetRandomKanji";
 
 const RandomKanji = ({ kanji }) => {
   const [randomKanji, setRandomKanji] = useState({});
+
+  const handleRandomRefresh = () => {
+    GetRandomKanji(kanji, setRandomKanji);
+  };
 
   useEffect(() => {
     function fetchRandomKanji() {
@@ -16,18 +20,23 @@ const RandomKanji = ({ kanji }) => {
   return (
     <div className="RandomKanji">
       {randomKanji !== {} && (
-        <KanjiCard
-          kanji={randomKanji.kanji}
-          key={randomKanji.kanji}
-          grade={randomKanji.grade}
-          meanings={randomKanji.meanings}
-          heisig={randomKanji.heisig_en}
-          kun={randomKanji.kun_readings}
-          on={randomKanji.on_readings}
-          stroke={randomKanji.stroke_count}
-          jlpt={randomKanji.jlpt}
-          name={randomKanji.name_readings}
-        />
+        <>
+          <KanjiCard
+            kanji={randomKanji.kanji}
+            key={randomKanji.kanji}
+            grade={randomKanji.grade}
+            meanings={randomKanji.meanings}
+            heisig={randomKanji.heisig_en}
+            kun={randomKanji.kun_readings}
+            on={randomKanji.on_readings}
+            stroke={randomKanji.stroke_count}
+            jlpt={randomKanji.jlpt}
+            name={randomKanji.name_readings}
+          />
+          <p className="refreshRandom" onClick={handleRandomRefresh}>
+            Click for another kanji!
+          </p>
+        </>
       )}
     </div>
   );
