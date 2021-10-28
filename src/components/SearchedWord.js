e467826dd692530be36377876b9c2bcb9710a945
Dean Spooner <dean.spooner@me.com>
2021-10-28 14:25:41 +0100

    Added functionality to click on an individual kanji after English word search to link to its kanji card.

--- src/components/SearchedWord.js
+++ src/components/SearchedWord.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { kanaArray } from "../data/Kana";
+import KanjiMiniCard from "./KanjiMiniCard";
 import GetSearchedWord from "../requests/GetSearchedWord";
 
 const SearchedWord = ({ searchedWord }) => {
@@ -34,6 +37,40 @@ const SearchedWord = ({ searchedWord }) => {
         {wordResults.length === 0
           ? "Ukana cannot find your word. Please check your spelling!"
           : wordResults[0].senses[0].english_definitions[0]}
+      </p>
+      <p>
+        Click on a kanji for its info:{" "}
+        {wordResults[0].slug.split("").map(
+          kanji =>
+            kanaArray.filter(e => e.hiragana === kanji).length === 0 &&
+            kanaArray.filter(e => e.katakana === kanji).length === 0 &&
+            kanji !== "ゃ" &&
+            kanji !== "ゅ" &&
+            kanji !== "ょ" &&
+            kanji !== "ぁ" &&
+            kanji !== "ぃ" &&
+            kanji !== "ぅ" &&
+            kanji !== "ぇ" &&
+            kanji !== "ぉ" &&
+            kanji !== "っ" &&
+            kanji !== "ャ" &&
+            kanji !== "ュ" &&
+            kanji !== "ョ" &&
+            kanji !== "ァ" &&
+            kanji !== "ィ" &&
+            kanji !== "ゥ" &&
+            kanji !== "ェ" &&
+            kanji !== "ォ" &&
+            kanji !== "ッ" && (
+              <Link
+                className="item non-home"
+                to={{ pathname: "/ukana/kanji", state: { kanji: kanji } }}
+                key={kanji}
+              >
+                <KanjiMiniCard kanji={kanji} />
+              </Link>
+            )
+        )}
       </p>
     </div>
   );
